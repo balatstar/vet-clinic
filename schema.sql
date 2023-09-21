b36c050a92262375cfa2eec6824d978167932c72
joyce <joyce.ann.cruz@gmail.com>
2023-09-21 16:01:10 +0800

    Query multiple tables

--- schema.sql
+++ schema.sql
@@ -11,3 +11,30 @@ CREATE TABLE animals (
 );
 ALTER TABLE animals
 ADD COLUMN species VARCHAR(50);
+
+CREATE TABLE owners (
+  id integer GENERATED ALWAYS AS IDENTITY,
+  full_name varchar(100),
+  age integer,
+  PRIMARY KEY(id)
+);
+
+CREATE TABLE species (
+  id integer GENERATED ALWAYS AS IDENTITY,
+  name varchar(50),
+  PRIMARY KEY(id)
+);
+
+ALTER TABLE animals
+ADD COLUMN species_id integer,
+ADD CONSTRAINT fk_species
+    FOREIGN KEY (species_id)
+    REFERENCES species(id);
+
+ALTER TABLE animals
+ADD COLUMN owner_id integer,
+ADD CONSTRAINT fk_owner
+    FOREIGN KEY (owner_id)
+    REFERENCES owners(id);
+
+ALTER TABLE animals DROP COLUMN species;
